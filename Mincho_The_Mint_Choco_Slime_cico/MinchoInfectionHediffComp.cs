66bf2a0f9c1802b1a35c8d25ceb1144ff4fab2f7
cico4450 <cico4450@gmail.com>
2020-05-25 01:22:47 +0900

    cico

---
+++ Mincho_The_Mint_Choco_Slime_cico/MinchoInfectionHediffComp.cs
@@ -0,0 +1,32 @@
+﻿using RimWorld;
+using System;
+using Verse;
+
+namespace Mincho_Infection
+{
+	// Token: 0x02000003 RID: 3
+	public class MinchoInfectionHediffComp : HediffComp
+	{
+		// Token: 0x06000004 RID: 4 RVA: 0x00002414 File Offset: 0x00000614
+		public override void CompPostTick(ref float severityAdjustment)
+		{
+			base.CompPostTick(ref severityAdjustment);
+
+
+			//if (this.parent.Severity >= 0.75f & !this.finalStage)
+			//{
+			//this.parent.comps.RemoveAll((HediffComp x) => x is HediffComp_TendDuration);
+			//this.finalStage = true;
+			//}
+			if (base.parent.Severity >= 0.01f)
+			{
+				//base.parent.comps.RemoveAll((HediffComp x) => x is HediffComp_TendDuration);
+			}
+			if (base.parent.Severity >= 1.00f)
+			{
+				//base.Pawn.health.Reset();
+				MinchoGenerator.GenerateMincho(base.Pawn, this.parent);
+			}
+		}
+	}
+}
